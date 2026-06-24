92247a2e8f07121d4b9e586f20e6bf6c4ad022f9
infinite-alex <infinite.alex5@gmail.com>
2026-06-24 12:43:14 +0300

    first commit of the project

---
+++ src/test/java/timesheet_management_system/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package timesheet_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
